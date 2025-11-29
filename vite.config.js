f220ff91b8d0fcefce8780680e4a5db0096b4d43
mohammedabsal <mohammedabsal9943@gmail.com>
2025-11-30 00:49:06 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base: '/Travel-Tour/',
+  plugins: [react(), tailwindcss()],
+  // base should match the GitHub Pages repository path (homepage in package.json)
+  base: '/Travel-Tours/',
 })
